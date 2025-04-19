e4eb960b277dec7cfaa930202052a5c8d14af9c5
Lucas1Ferreira <lufebetico09@gmail.com>
2025-04-19 10:58:17 -0300

    Finalizafo a classe AzureIotConnection

--- conection/AzureConection.py
+++ conection/AzureConection.py
@@ -1,17 +1,29 @@
 import time
-import rando 
+import random
 from azure.iot.device import IoTHubDeviceClient, Message
 
+class AzureIotConnection:
+    def __init__(self, connection_string=None):
+        if connection_string is None:
+            self.connection_string = "HostName=Grupo09.azure-devices.net;DeviceId=TesteConectsSensor;SharedAccessKey=W2T8NbTMa7mN+86/Had4O3DbTudxACoKYJLNFXphuns="
+        else:
+            self.connection_string = connection_string
+        self.device_client = None
 
-class AzureConnection:
+    def connect(self):
+        self.device_client = IoTHubDeviceClient.create_from_connection_string(self.connection_string)
+        self.device_client.connect()
+        print("Conectado ao Azure IoT Hub")
 
-    def connect(self, Connection_String = none, device_client = none, message = none):
-        Connection_String = "HostName=Grupo09.azure-devices.net;DeviceId=TesteConectsSensor;SharedAccessKey=W2T8NbTMa7mN+86/Had4O3DbTudxACoKYJLNFXphuns="
-        device_client = IoTHubDeviceClient.create_from_connection_string(Connection_String)
-        device_client.connect()
+    def disconnect(self):
+        if self.device_client:
+            self.device_client.disconnect()
+            print("Desconectado do Azure IoT Hub")
 
-
-    def disconnect(self, device_client):
-        device_client.disconnect()
-        print("Disconnected from Azure IoT Hub")
-
+    def send_message(self, data):
+        if self.device_client:
+            message = Message(data)
+            message.content_encoding = "utf-8"
+            message.content_type = "application/json"
+            self.device_client.send_message(message)
+            print(f"Mensagem enviada: {data}")
